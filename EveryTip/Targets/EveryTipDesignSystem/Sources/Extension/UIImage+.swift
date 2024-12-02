a01bfac9290d736ce4fe74446b7a9fdaff6bf286
KimRoks <kimroks3161@gmail.com>
2024-12-02 17:19:59 +0900

    add: 정렬 버튼 아이콘 이미지 추가 및 extension 코드 작성

--- EveryTip/Targets/EveryTipDesignSystem/Sources/Extension/UIImage+.swift
+++ EveryTip/Targets/EveryTipDesignSystem/Sources/Extension/UIImage+.swift
@@ -12,6 +12,9 @@ public enum ImageAssetType {
     case homeViewEmoji
     case categoryViewBanner
     case blankImage
+    case sortImage_latest
+    case sortImage_likes
+    case sortImage_views
 }
 
 extension UIImage {
@@ -21,6 +24,12 @@ extension UIImage {
         case .homeViewEmoji: EveryTipDesignSystemAsset.homeViewEmoji.image
         case .categoryViewBanner: EveryTipDesignSystemAsset.categoryViewBanner.image
         case .blankImage: EveryTipDesignSystemAsset.blankImage.image
+        case .sortImage_latest:
+            EveryTipDesignSystemAsset.sortImageLatest.image
+        case .sortImage_likes:
+            EveryTipDesignSystemAsset.sortImageLikes.image
+        case .sortImage_views:
+            EveryTipDesignSystemAsset.sortImageViews.image
         }
     }
 }
